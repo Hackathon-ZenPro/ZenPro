a93d57e21fd5652acf9a83a09bfe40f3616e4373
ChrisJabb21 <chrisjabbour94@gmail.com>
2020-10-07 13:06:52 -0400

    added public and src folders (untracked files) to frontend and started rest api implementation

--- hackathon-backend/src/main/java/com/cognixia/hackathon/controller/HackathonController.java
+++ hackathon-backend/src/main/java/com/cognixia/hackathon/controller/HackathonController.java
@@ -1,10 +1,14 @@
 package com.cognixia.hackathon.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import javassist.NotFoundException;
 
 import com.cognixia.hackathon.dao.OrderDAO;
 import com.cognixia.hackathon.dao.ProductDAO;
@@ -12,6 +16,7 @@ import com.cognixia.hackathon.dao.SearchedDAO;
 import com.cognixia.hackathon.dao.ShoppingCartDAO;
 import com.cognixia.hackathon.dao.UserDAO;
 import com.cognixia.hackathon.dao.ViewedDAO;
+import com.cognixia.hackathon.model.Product;
 import com.cognixia.hackathon.model.User;
 
 @RestController
@@ -41,6 +46,27 @@ public class HackathonController {
 		System.out.println(ViewedRepo.findAll());
 		return userRepo.findAll();  //   http://localhost:8080/
 	}
+	@GetMapping("/users")
+	public List<User> getAllUsers() {
+		return userRepo.findAll();
+	}
+	
+	@GetMapping("/products")
+	public List<Product> getAllProducts() {
+		return productRepo.findAll();
+	}
+
+
+	@GetMapping("/users/{id}")
+	public User getUserById(@PathVariable int id) throws NotFoundException {
+		Optional<User> user = userRepo.findById(id);
+
+		if(!user.isPresent())
+			throw new NotFoundException("User with id=" + id +" cannot be found");
+
+		return user.get();
+	}
+
 
 	
 }
