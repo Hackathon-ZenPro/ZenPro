4be90c10a0cac5a01ae0670f450f54b31d6bbbf7
Mathias <mathiasjtaylor@gmail.com>
2020-10-08 14:23:00 -0700

    Added bootstrap

--- react-frontend-ui/src/App.js
+++ react-frontend-ui/src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 import { Switch, BrowserRouter, Route } from 'react-router-dom';
 import Home from "./components/Home"
@@ -9,12 +8,17 @@ import Shop from "./components/Shop"
 import Events from "./components/Events"
 import ShoppingCart from "./components/ShoppingCart"
 import UserInfo from './components/UserInfo'
+import {  Container } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <>
+<div className="homePage">
+<Container fluid>
       <BrowserRouter /*style={{justifyContent: "center",alignItems: "center"}}*/>
         <Navbar/>
+        
             <Switch>
               <Route path='/shop' component={Shop}  />
               <Route path='/events' component={Events}/>
@@ -22,8 +26,11 @@ function App() {
               <Route path='/userinfo' component={UserInfo}/>
               <Route path='/'  component={Home}/> {/* Right after log in, user will go here, all following paths will be buttons on the home screen  */}
             </Switch>
+            
           </BrowserRouter>
-      <Hero />
+          </Container>
+      </div>
+      
     </>
 
   );
