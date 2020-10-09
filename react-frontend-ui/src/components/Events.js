8e6fcca021fa174c8138496fbb19f2374d2e69d2
Darreal Chambers <dchambers@Darreals-MBP.lan1>
2020-10-08 20:45:51 -0400

    calendar formatted to right of page

--- react-frontend-ui/src/components/Events.js
+++ react-frontend-ui/src/components/Events.js
@@ -3,23 +3,53 @@ import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
  
 class Events extends Component {
-  state = {
-    date: new Date(),
-  }
+    constructor(props){
+        super(props)
+        this.state={
+            date: new Date(),
+            events:[]
+        }
+    }
+
+    // componentDidMount(){
+    //     this.findProducts()
+    // }
+
+    // findProducts(){
+    //     var url = "http://localhost:8080/api/products"
+    //     axios.get(url)
+    //         .then(response => response.data)
+    //         .then((data) => {
+    //             this.setState({products:data})
+    //             console.log(data)
+    //         })
+    // }
  
-  onChange = date => this.setState({ date })
- 
-  render() {
-    return (
-      <div>
-        <Calendar
-          onChange={this.onChange}
-          value={this.state.date}
-          minDate={new Date()}
-        />
-      </div>
-    );
-  }
+    onChange = date => this.setState({ date })
+    
+    render() {
+        return (
+        <div>
+
+            <div className="sticky">
+                <h4 style={{textAlign:"center"}}>Event Calendar View</h4>
+                <Calendar
+                    onChange={this.onChange}
+                    value={this.state.date}
+                    minDate={new Date()}
+                />
+                <h4 style={{textAlign:"center"}}>Become apart of the ZenPro family!!</h4>
+                <h4 style={{textAlign:"center"}}>Gain exclusive deals for events</h4>
+                <h4 style={{textAlign:"center"}}>Videochat with ZenPro event planners</h4>
+                <h4 style={{textAlign:"center"}}>To learn more click the Sign Up button</h4>
+            </div>
+
+            <div style={{marginLeft:"5%", width:"80%"}}>
+                <h1>Events</h1>
+            </div>
+        </div>
+        );
+    }
 }
 
 export default Events;
