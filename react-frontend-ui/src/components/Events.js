c608966a0a945d1b4f84faca890b84b7464af40e
Darreal Chambers <dchambers@Darreals-MBP.lan1>
2020-10-11 22:29:48 -0400

    Event signup form desgin complete

--- react-frontend-ui/src/components/Events.js
+++ react-frontend-ui/src/components/Events.js
@@ -1,4 +1,4 @@
-import React, { Component, useState } from 'react';
+import React, { Component } from 'react';
 import axios from 'axios';
 import Calendar from 'react-calendar';
 import Modal from 'react-modal';
@@ -36,7 +36,57 @@ class Events extends Component {
         return (
         <div>
             <Modal isOpen={this.state.isModalOpen} onRequestClose={this.closeModal}>
-                <button onClick={this.closeModal}>Close</button>
+                <h1 style={{textAlign:"center", color:"#006555", fontFamily:"Roboto Slab, serif", textDecoration:"underline"}}>Event Sign Up Form</h1>
+                <br/>
+                <form>
+                    <div class="form-row">
+                        <div class="form-group col-md-6">
+                            <label for="inputEmail4" className="label">First Name</label>
+                            <input type="text" class="form-control" id="fname" placeholder="First Name"/>
+                        </div>
+                        <div class="form-group col-md-6">
+                            <label for="inputPassword4" className="label">Last Name</label>
+                            <input type="text" class="form-control" id="lname" placeholder="Last Name"/>
+                        </div>
+                    </div>
+                    <div class="form-row">
+                        <div class="form-group col-md-6">
+                            <label for="inputEmail4" className="label">Email</label>
+                            <input type="email" class="form-control" id="inputEmail4" placeholder="Email"/>
+                        </div>
+                        <div class="form-group col-md-6">
+                            <label for="inputPassword4" className="label">Password</label>
+                            <input type="password" class="form-control" id="inputPassword4" placeholder="Password"/>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="inputAddress" className="label">Address</label>
+                        <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St"/>
+                    </div>
+                    <div class="form-row">
+                        <div class="form-group col-md-6">
+                            <label for="inputCity" className="label">City</label>
+                            <input type="text" class="form-control" id="inputCity" placeholder="City"/>
+                        </div>
+                        <div class="form-group col-md-4">
+                            <label for="inputState" className="label">State</label>
+                            <input type="text" class="form-control" id="inputState" placeholder="State"/>
+                        </div>
+                        <div class="form-group col-md-2">
+                            <label for="inputZip" className="label">Zip</label>
+                            <input type="text" class="form-control" id="inputZip" placeholder="#####"/>
+                        </div>
+                    </div>
+                    <div class="form-row">
+                        <div class="form-group col-md-11">
+                            <button type="submit" class="btn btn-primary">Sign in</button>
+                        </div>
+                        <div class="form-group col-md-0">
+                            <button className="btn btn-danger" onClick={this.closeModal}>Close</button>
+                        </div>
+                    </div>
+                </form>
+                <img src="https://tinyurl.com/y67yp5xs" alt="Thank You" style={{width:"100%", height:"30%"}}/>
             </Modal>
             <br/>
             <div className="sticky">
@@ -55,7 +105,7 @@ class Events extends Component {
             </div>
 
             <div style={{marginLeft:"5%", width:"80%"}}>
-                <h1 style={{textAlign:"center", color:"#006555", fontFamily:"Roboto Slab, serif", textDecoration:"underline"}}>ZenPro Events</h1><br/>
+                <h1 style={{textAlign:"center", color:"#006555", fontFamily:"Roboto Slab, serif", textDecoration:"underline"}}>ZenPro Upcoming Events</h1><br/>
 
                 {this.state.events.map((event) => (
                     <div id={event.eventId}>
@@ -65,8 +115,8 @@ class Events extends Component {
                                     <td><img src={event.eventImg} alt ="Api error" style ={{height:"250px", width:"450px", border:"3px solid #006555"}}/></td>
                                     <td style={{width:"550px", textAlign:"center"}}>
                                         <h5 style={{textAlign:"center", fontWeight:"bold"}}>{event.eventName}</h5>
-                                        <h5 style={{color:"#006555", fontStyle:"italic"}}>{event.eventDescription}For more information on how to join or any other questions please use the following link.</h5>
-                                        <button className="btn btn-success" onClick={this.openModal}>Join Now!!</button>
+                                        <h5 style={{color:"#006555", fontStyle:"italic"}}>{event.eventDescription} To attend this event please click the Sign Up button below and fill out the provided information.</h5>
+                                        <button className="btn btn-success" onClick={this.openModal} id={event.eventId}>Sign Up!!</button>
                                         <h5 style={{fontWeight:"bold"}}>Event Details: {event.eventDate}</h5>
                                     </td>
                                 </tr>
